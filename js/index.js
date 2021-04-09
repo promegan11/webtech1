df59f4dcbd8caa80ea85364542012d2ffdf8a72f
promegan11 <promegan11@gmail.com>
2021-04-09 15:28:50 +0200

    as

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
-$(document).ready(function (){
+$(document).ready(function () {
     $("#content").load("home.html");
-    $.each($(".menuButton"),function (mbIndex,mbValue){
-            $(mbValue).click(function (event){
-                event.preventDefault();
-                if ($(this).find('a').attr("href")=="index.html"){
-                    open("index.html","_self")
-                }
-                else{
-                    $("#content").load($(this).find('a').attr("href"));
-                }
-            })
+    $.each($(".menuButton"), function (mbIndex, mbValue) {
+        $(mbValue).click(function (event) {
+            event.preventDefault();
+            if ($(this).find('a').attr("href") == "index.html") {
+                open("index.html", "_self")
+            } else {
+                $("#content").load($(this).find('a').attr("href"));
+            }
+        })
     })
 })
